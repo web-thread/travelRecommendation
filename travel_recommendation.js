59a38d2810f513bd0b5588ea747ea9ef5739b8c8
Alex Pertsi Git <alexpertsi@gmail.com>
2025-04-29 14:16:12 +0300

    Release canditate

---
+++ travel_recommendation.js
@@ -0,0 +1,85 @@
+const btnSearch = document.getElementById('btnSearch');
+const btnReset = document.getElementById('btnReset');
+const result = document.getElementById('result');
+const keywordInput = document.getElementById('searchInput');
+let options = { timeZone: 'America/New_York', hour12: true, hour: '2-digit', minute: '2-digit' };
+
+// Initially disable the buttons
+btnSearch.disabled = true;
+btnReset.disabled = true;
+
+// Enable buttons only if input has value
+keywordInput.addEventListener('input', () => {
+  const hasValue = keywordInput.value.trim() !== '';
+  btnSearch.disabled = !hasValue;
+  btnReset.disabled = !hasValue;
+});
+
+
+function searchKeyword() {
+    const keyword = keywordInput.value.toLowerCase().trim();
+    result.innerHTML = '';
+
+    fetch('travel_recommendation_api.json')
+      .then(response => response.json())
+      .then(data => {
+
+        result.classList.add('show');
+
+        let results = [];
+
+        if (keyword === 'beach' || keyword === 'beaches') {
+          results = data.beaches;
+        } else if (keyword === 'temple' || keyword === 'temples') {
+          results = data.temples;
+        } else {
+          // Look for matching country
+          const countryMatch = data.countries.find(c =>
+            c.name.toLowerCase() === keyword
+          );
+          if (countryMatch) {
+            results = countryMatch.cities;
+          }
+        }
+      
+        if (results.length === 0) {
+          result.innerHTML = `<p><strong>No results found</strong>. <br>Try 'beach', 'temple', or a country like 'Japan'.</p>`;
+          return;
+        }
+
+        results.forEach(place => {
+          options.timeZone = place.timezone;
+          let placeTime = new Date().toLocaleTimeString('en-US', options);
+          const card = document.createElement('div');
+          card.className = 'card';
+          card.innerHTML = `
+            <img src="${place.imageUrl}" alt="${place.name}">
+            <h3>${place.name}</h3>
+            <span class="time">Current time: ${placeTime}</span>
+            <p>${place.description}</p>
+          `;
+          result.appendChild(card);
+        });
+
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        result.innerHTML = 'An error occurred while fetching data.';
+      });
+}
+btnSearch.addEventListener('click', searchKeyword);
+
+searchInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter' && searchInput.value.trim() !== '') {
+    event.preventDefault(); // Prevent form submission if in a form
+    searchKeyword();        // Call your search function
+  }
+});
+
+
+btnReset.addEventListener('click', () => {
+  result.classList.remove('show');
+  keywordInput.value = '';
+  btnSearch.disabled = true;
+  btnReset.disabled = true;
+});
